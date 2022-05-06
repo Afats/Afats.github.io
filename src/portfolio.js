2c55331bbe94f8707c2d5fa6f7afc26ffc66d88d
Afats <mmdohad@gmail.com>
2022-05-06 22:55:01 +1000

    moose.

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,6 +1,6 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://localhost:8000',
+  homepage: 'https://en.wikipedia.org/wiki/Moose',
   title: 'Moose.',
 }
 
